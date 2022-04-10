e050745835b88115df7a9d77f045148a31b6a8e1
PabloMelo <pablomelodev@gmail.com>
2022-04-10 11:23:23 -0300

    feat: send message when create a new purchase

--- purchases/src/services/purchases.service.ts
+++ purchases/src/services/purchases.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
+
 import { PrismaService } from '../database/prisma/prisma.service';
+import { KafkaService } from '../messaging/kafka/kafka.service';
 
 type CreatePurchaseParams = {
   customerId: string;
@@ -8,7 +10,7 @@ type CreatePurchaseParams = {
 
 @Injectable()
 export class PurchasesService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private kafka: KafkaService) {}
 
   async listAllPurchases() {
     return this.prisma.purchase.findMany({
@@ -34,11 +36,34 @@ export class PurchasesService {
       throw new Error('Product not found.');
     }
 
-    return await this.prisma.purchase.create({
+    const customer = await this.prisma.customer.findUnique({
+      where: {
+        id: customerId,
+      },
+    });
+
+    if (!customer) {
+      throw new Error('Customer not found.');
+    }
+
+    const purchase = await this.prisma.purchase.create({
       data: {
         customerId,
         productId,
       },
     });
+
+    this.kafka.emit('purchases.new-purchase', {
+      customer: {
+        authUserId: customer.authUserId,
+      },
+      product: {
+        id: product.id,
+        title: product.title,
+        slug: product.slug,
+      },
+    });
+
+    return purchase;
   }
 }
