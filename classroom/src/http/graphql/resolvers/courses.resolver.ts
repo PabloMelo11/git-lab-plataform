54a37e3fd3e7f3df135e2de5f05186cf582f69f8
PabloMelo <pablomelodev@gmail.com>
2022-04-09 21:54:25 -0300

    feat: list all courses

--- classroom/src/http/graphql/resolvers/courses.resolver.ts
+++ classroom/src/http/graphql/resolvers/courses.resolver.ts
@@ -1,6 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query } from '@nestjs/graphql';
+
+import { AuthorizationGuard } from '../../auth/authorization.guard';
+
+import { CoursesService } from '../../../services/courses.service';
 
 import { Course } from '../models/course';
 
 @Resolver(() => Course)
-export class CoursesResolver {}
+export class CoursesResolver {
+  constructor(private coursesService: CoursesService) {}
+
+  @Query(() => [Course])
+  @UseGuards(AuthorizationGuard)
+  async courses() {
+    return await this.coursesService.listAllCourses();
+  }
+}
