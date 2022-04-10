1633e8a38828f7bf6f0536a2a7cb2f9ede5af1e2
PabloMelo <pablomelodev@gmail.com>
2022-04-10 11:19:10 -0300

    feat: setup kafka

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 
 import { DatabaseModule } from '../database/database.module';
+import { MessagingModule } from '../messaging/messaging.module';
 
 import { ProductsResolver } from './graphql/resolvers/products.resolver';
 import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
@@ -18,6 +19,7 @@ import { CustomersService } from '../services/customers.service';
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
+    MessagingModule,
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
